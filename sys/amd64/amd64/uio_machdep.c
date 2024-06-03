74c879c75f5bb86e0e95250a2b768a339c50a29e
Brooks Davis <brooks@FreeBSD.org>
2024-06-03 23:42:36 +0100

    uiomove_fromphys: use IOVEC_ADVANCE
    Slightly simplify and reduce diffs to CheriBSD.
    
    Obtained from:	CheriBSD
    Sponsored by:	DARPA, AFRL

--- sys/amd64/amd64/uio_machdep.c
+++ sys/amd64/amd64/uio_machdep.c
@@ -126,8 +126,7 @@ uiomove_fromphys(vm_page_t ma[], vm_offset_t offset, int n, struct uio *uio)
 			    &vaddr, 1, true);
 			mapped = false;
 		}
-		iov->iov_base = (char *)iov->iov_base + cnt;
-		iov->iov_len -= cnt;
+		IOVEC_ADVANCE(iov, cnt);
 		uio->uio_resid -= cnt;
 		uio->uio_offset += cnt;
 		offset += cnt;
