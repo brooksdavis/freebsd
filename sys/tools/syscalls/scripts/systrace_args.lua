agge3/freebsd-src

Refactor makesyscalls.lua into a library
* main.lua replicates the functionality of makesyscalls.lua
* Individual files are generated by their associated module
  * Modules can be called as standalone scripts to generate a specific
    file
* Data and procedures are performed by objects instead of procedual code
* Bitmasks are replaced by declarative types
* Temporary files are no longer produced, writing is stored in memory
* Comments provide explanation to functions and semantics

Google Summer of Code 2024 Final Work Product

Sponsored by:    Google (GSoC 24)
Co-authored-by: Warner Losh <imp@FreeBSD.org>
Co-authored-by: Brooks Davis <brooks@FreeBSD.org>
Pull Request:	https://github.com/freebsd/freebsd-src/pull/1362

sys/tools/syscalls/scripts/systrace_args.lua
@@ -1,0 +1,267 @@
+#!/usr/libexec/flua
+--
+-- SPDX-License-Identifier: BSD-2-Clause
+--
+-- Copyright (c) 2024 Tyler Baxter <agge@FreeBSD.org>
+-- Copyright (c) 2023 Warner Losh <imp@bsdimp.com>
+-- Copyright (c) 2019 Kyle Evans <kevans@FreeBSD.org>
+--
+
+-- Setup to be a module, or ran as its own script.
+local systrace_args = {}
+local script = not pcall(debug.getlocal, 4, 1)	-- TRUE if script.
+if script then
+	-- Add library root to the package path.
+	local path = arg[0]:gsub("/[^/]+.lua$", "")
+	package.path = package.path .. ";" .. path .. "/../?.lua"
+end
+
+local FreeBSDSyscall = require("core.freebsd-syscall")
+local util = require("tools.util")
+local generator = require("tools.generator")
+
+-- File has not been decided yet; config will decide file.
+systrace_args.file = "/dev/null"
+
+function systrace_args.generate(tbl, config, fh)
+	-- Grab the master system calls table.
+	local s = tbl.syscalls
+
+	-- Bind the generator to the parameter file.
+	local gen = generator:new({}, fh)
+	gen.storage_levels = {} -- make sure storage is clear
+
+	-- 64-bit padding preprocessor directive.
+	gen:pad64(config.abiChanges("pair_64bit"))
+
+	-- Write the generated preamble.
+	gen:preamble(
+	    "System call argument to DTrace register array converstion.\n" ..
+	    "\n" ..
+	    "This file is part of the DTrace syscall provider.")
+
+	gen:write(string.format([[
+static void
+systrace_args(int sysnum, void *params, uint64_t *uarg, int *n_args)
+{
+	int64_t *iarg = (int64_t *)uarg;
+	int a = 0;
+	switch (sysnum) {
+]]))
+
+	gen:store(string.format([[
+static void
+systrace_entry_setargdesc(int sysnum, int ndx, char *desc, size_t descsz)
+{
+	const char *p = NULL;
+	switch (sysnum) {
+]]), 1)
+
+	gen:store(string.format([[
+static void
+systrace_return_setargdesc(int sysnum, int ndx, char *desc, size_t descsz)
+{
+	const char *p = NULL;
+	switch (sysnum) {
+]]), 2)
+
+	for _, v in pairs(s) do
+
+		-- Handle non compat:
+		if v:native() then
+			gen:write(string.format([[
+	/* %s */
+	case %d: {
+]], v.name, v.num))
+
+			gen:store(string.format([[
+	/* %s */
+	case %d:
+]], v.name, v.num), 1)
+
+			gen:store(string.format([[
+	/* %s */
+	case %d:
+]], v.name, v.num), 2)
+
+			local n_args = #v.args
+			if v.type.SYSMUX then
+				n_args = 0
+			end
+
+			if #v.args > 0 and not v.type.SYSMUX then
+				local padding = ""
+
+				gen:write(string.format([[
+		struct %s *p = params;
+]],
+				    v.arg_alias))
+
+				gen:store([[
+		switch (ndx) {
+]],
+				    1)
+
+				for idx, arg in ipairs(v.args) do
+					local argtype = util.trim(
+						arg.type:gsub(
+						    "__restrict$", ""), nil)
+					if argtype == "int" and
+					    arg.name == "_pad" and
+					    config.abiChanges("pair_64bit") then
+						gen:store(
+						    "#ifdef PAD64_REQUIRED\n",
+						    1)
+					end
+
+					-- Pointer arg?
+					local desc
+					if argtype:find("*") then
+						desc = "userland " .. argtype
+					else
+						desc = argtype;
+					end
+
+					gen:store(string.format([[
+		case %d%s:
+			p = "%s";
+			break;
+]],
+					    idx - 1, padding, desc), 1)
+
+					if argtype == "int" and
+					    arg.name == "_pad" and
+					   config.abiChanges("pair_64bit") then
+						padding = " - _P_"
+						gen:store([[
+#define _P_ 0
+#else
+#define _P_ 1
+#endif
+]],
+					    1)
+					end
+
+					if util.isPtrType(argtype,
+					    config.abi_intptr_t) then
+						gen:write(string.format([[
+		uarg[a++] = (%s)p->%s; /* %s */
+]],
+						    config.ptr_intptr_t_cast,
+						    arg.name, argtype))
+					elseif argtype == "union l_semun" then
+						gen:write(string.format([[
+		uarg[a++] = p->%s.buf; /* %s */
+]],
+						    arg.name, argtype))
+					elseif argtype:sub(1,1) == "u" or
+					    argtype == "size_t" then
+						gen:write(string.format([[
+		uarg[a++] = p->%s; /* %s */
+]],
+						    arg.name, argtype))
+					else
+						if argtype == "int" and
+						    arg.name == "_pad" and
+						    config.abiChanges(
+						    "pair_64bit") then
+							gen:write([[
+#ifdef PAD64_REQUIRED
+]])
+						end
+
+						gen:write(string.format([[
+		iarg[a++] = p->%s; /* %s */
+]],
+						    arg.name, argtype))
+
+						if argtype == "int" and
+						    arg.name == "_pad" and
+						    config.abiChanges(
+						    "pair_64bit") then
+							gen:write("#endif\n")
+						end
+					end
+				end
+
+				gen:store([[
+		default:
+			break;
+		};
+]],
+				    1)
+
+				if padding ~= "" then
+					gen:store("#undef _P_\n\n", 1)
+				end
+
+				gen:store(string.format([[
+		if (ndx == 0 || ndx == 1)
+			p = "%s";
+		break;
+]], v.ret), 2)
+		end
+
+		gen:write(string.format("\t\t*n_args = %d;\n\t\tbreak;\n\t}\n",
+		    n_args))
+		gen:store("\t\tbreak;\n", 1)
+
+		-- Handle compat (everything >= FREEBSD3):
+		-- Do nothing, only for native.
+		end
+	end
+
+	gen:write([[
+	default:
+		*n_args = 0;
+		break;
+	};
+}
+]])
+	gen:store([[
+	default:
+		break;
+	};
+	if (p != NULL)
+		strlcpy(desc, p, descsz);
+}
+]], 1)
+	gen:store([[
+	default:
+		break;
+	};
+	if (p != NULL)
+		strlcpy(desc, p, descsz);
+}
+]], 2)
+
+	-- Write all stored lines.
+	if gen.storage_levels ~= nil then
+		gen:writeStorage()
+	end
+
+end
+
+-- Entry of script:
+if script then
+	local config = require("config")
+
+	if #arg < 1 or #arg > 2 then
+		error("usage: " .. arg[0] .. " syscall.master")
+	end
+
+	local sysfile, configfile = arg[1], arg[2]
+
+	config.merge(configfile)
+	config.mergeCompat()
+	config.mergeCapability()
+
+	-- The parsed system call table.
+	local tbl = FreeBSDSyscall:new{sysfile = sysfile, config = config}
+
+	systrace_args.file = config.systrace -- change file here
+	systrace_args.generate(tbl, config, systrace_args.file)
+end
+
+-- Return the module.
+return systrace_args

sys/tools/syscalls/tools/generator.lua
@@ -1,0 +1,112 @@
+--
+-- SPDX-License-Identifier: BSD-2-Clause
+--
+-- Copyright (c) 2024 Tyler Baxter <agge@FreeBSD.org>
+-- Copyright (c) 2019 Kyle Evans <kevans@FreeBSD.org>
+--
+
+local util = require("tools.util")
+
+local generator = {}
+
+generator.__index = generator
+
+-- Wrapper for lua write() best practice. For a simpler write call.
+function generator:write(line)
+	assert(self.gen:write(line))
+end
+
+--
+-- A write macro for the PAD64 preprocessor directive.
+-- PARAM: bool, TRUE to pad
+-- USAGE: Pass config.abiChanges("pair_64bit") and padding will be done if
+-- necessary. Useful for 32-bit configurations.
+--
+function generator:pad64(bool)
+	if bool then
+		self:write([[
+#if !defined(PAD64_REQUIRED) && !defined(__amd64__)
+#define PAD64_REQUIRED
+#endif
+]])
+	end
+end
+
+-- Returns the generated tag.
+function generator:tag()
+	return self.tag
+end
+
+generator.storage_levels = {}
+
+-- Optional level to specify which order to store in, which defaults to one if
+-- not provided.
+function generator:store(str, level)
+	level = level or 1
+	self.storage_levels[level] = self.storage_levels[level] or {}
+	table.insert(self.storage_levels[level], str)
+end
+
+-- Write all storage in the order it was stored.
+function generator:writeStorage()
+	if self.storage_levels ~= nil then
+		for _, v in util.ipairs_sparse(self.storage_levels) do
+			for _, line in ipairs(v) do
+				generator:write(line)
+			end
+		end
+	end
+end
+
+--
+-- Writes the generated preamble. Default comment is C comments.
+--
+-- PARAM: String str, the title for the file.
+--
+-- PARAM: String comment, nil or optional to change comment (e.g., to sh
+-- comments).
+--
+-- SEE: style(9)
+--
+function generator:preamble(str, comment)
+	if str ~= nil then
+		local comment_start = comment or "/*"
+		local comment_middle = comment or " *"
+		local comment_end = comment or " */"
+		self:write(string.format("%s\n", comment_start))
+		-- Splits our string into lines split by newline, or is just the
+		-- original string if there's no newlines.
+		for line in str:gmatch("[^\n]*") do
+			-- Only add a space after the comment if there's
+			-- text on this line.
+			local space
+			if line ~= "" then
+				space = " "
+			else
+				space = ""
+			end
+			-- Make sure to append the newline back.
+			self:write(string.format("%s%s%s\n", comment_middle,
+			    space, line))
+		end
+		self:write(string.format([[%s
+%s DO NOT EDIT-- this file is automatically %s.
+%s
+
+]], comment_middle, comment_middle, self.tag, comment_end))
+	end
+end
+
+-- generator binds to the parameter file.
+function generator:new(obj, fh)
+	obj = obj or { }
+	setmetatable(obj, self)
+	self.__index = self
+
+	self.gen = assert(io.open(fh, "w+"))
+	self.tag = "@" .. "generated"
+
+	return obj
+end
+
+return generator

brooksdavis/freebsd

Co-authored-by: Kyle Evans <kevans@FreeBSD.org>
Signed-off-by: agge3 <sterspark@gmail.com>

sys/tools/syscalls/scripts/systrace_args.lua
@@ -1,0 +1,267 @@
+#!/usr/libexec/flua
+--
+-- SPDX-License-Identifier: BSD-2-Clause
+--
+-- Copyright (c) 2024 Tyler Baxter <agge@FreeBSD.org>
+-- Copyright (c) 2023 Warner Losh <imp@bsdimp.com>
+-- Copyright (c) 2019 Kyle Evans <kevans@FreeBSD.org>
+--
+
+-- Setup to be a module, or ran as its own script.
+local systrace_args = {}
+local script = not pcall(debug.getlocal, 4, 1)	-- TRUE if script.
+if script then
+	-- Add library root to the package path.
+	local path = arg[0]:gsub("/[^/]+.lua$", "")
+	package.path = package.path .. ";" .. path .. "/../?.lua"
+end
+
+local FreeBSDSyscall = require("core.freebsd-syscall")
+local util = require("tools.util")
+local generator = require("tools.generator")
+
+-- File has not been decided yet; config will decide file.
+systrace_args.file = "/dev/null"
+
+function systrace_args.generate(tbl, config, fh)
+	-- Grab the master system calls table.
+	local s = tbl.syscalls
+
+	-- Bind the generator to the parameter file.
+	local gen = generator:new({}, fh)
+	gen.storage_levels = {} -- make sure storage is clear
+
+	-- 64-bit padding preprocessor directive.
+	gen:pad64(config.abiChanges("pair_64bit"))
+
+	-- Write the generated preamble.
+	gen:preamble(
+	    "System call argument to DTrace register array conversion.\n" ..
+	    "\n" ..
+	    "This file is part of the DTrace syscall provider.")
+
+	gen:write(string.format([[
+static void
+systrace_args(int sysnum, void *params, uint64_t *uarg, int *n_args)
+{
+	int64_t *iarg = (int64_t *)uarg;
+	int a = 0;
+	switch (sysnum) {
+]]))
+
+	gen:store(string.format([[
+static void
+systrace_entry_setargdesc(int sysnum, int ndx, char *desc, size_t descsz)
+{
+	const char *p = NULL;
+	switch (sysnum) {
+]]), 1)
+
+	gen:store(string.format([[
+static void
+systrace_return_setargdesc(int sysnum, int ndx, char *desc, size_t descsz)
+{
+	const char *p = NULL;
+	switch (sysnum) {
+]]), 2)
+
+	for _, v in pairs(s) do
+
+		-- Handle non compat:
+		if v:native() then
+			gen:write(string.format([[
+	/* %s */
+	case %d: {
+]], v.name, v.num))
+
+			gen:store(string.format([[
+	/* %s */
+	case %d:
+]], v.name, v.num), 1)
+
+			gen:store(string.format([[
+	/* %s */
+	case %d:
+]], v.name, v.num), 2)
+
+			local n_args = #v.args
+			if v.type.SYSMUX then
+				n_args = 0
+			end
+
+			if #v.args > 0 and not v.type.SYSMUX then
+				local padding = ""
+
+				gen:write(string.format([[
+		struct %s *p = params;
+]],
+				    v.arg_alias))
+
+				gen:store([[
+		switch (ndx) {
+]],
+				    1)
+
+				for idx, arg in ipairs(v.args) do
+					local argtype = util.trim(
+						arg.type:gsub(
+						    "__restrict$", ""), nil)
+					if argtype == "int" and
+					    arg.name == "_pad" and
+					    config.abiChanges("pair_64bit") then
+						gen:store(
+						    "#ifdef PAD64_REQUIRED\n",
+						    1)
+					end
+
+					-- Pointer arg?
+					local desc
+					if argtype:find("*") then
+						desc = "userland " .. argtype
+					else
+						desc = argtype;
+					end
+
+					gen:store(string.format([[
+		case %d%s:
+			p = "%s";
+			break;
+]],
+					    idx - 1, padding, desc), 1)
+
+					if argtype == "int" and
+					    arg.name == "_pad" and
+					   config.abiChanges("pair_64bit") then
+						padding = " - _P_"
+						gen:store([[
+#define _P_ 0
+#else
+#define _P_ 1
+#endif
+]],
+					    1)
+					end
+
+					if util.isPtrType(argtype,
+					    config.abi_intptr_t) then
+						gen:write(string.format([[
+		uarg[a++] = (%s)p->%s; /* %s */
+]],
+						    config.ptr_intptr_t_cast,
+						    arg.name, argtype))
+					elseif argtype == "union l_semun" then
+						gen:write(string.format([[
+		uarg[a++] = p->%s.buf; /* %s */
+]],
+						    arg.name, argtype))
+					elseif argtype:sub(1,1) == "u" or
+					    argtype == "size_t" then
+						gen:write(string.format([[
+		uarg[a++] = p->%s; /* %s */
+]],
+						    arg.name, argtype))
+					else
+						if argtype == "int" and
+						    arg.name == "_pad" and
+						    config.abiChanges(
+						    "pair_64bit") then
+							gen:write([[
+#ifdef PAD64_REQUIRED
+]])
+						end
+
+						gen:write(string.format([[
+		iarg[a++] = p->%s; /* %s */
+]],
+						    arg.name, argtype))
+
+						if argtype == "int" and
+						    arg.name == "_pad" and
+						    config.abiChanges(
+						    "pair_64bit") then
+							gen:write("#endif\n")
+						end
+					end
+				end
+
+				gen:store([[
+		default:
+			break;
+		};
+]],
+				    1)
+
+				if padding ~= "" then
+					gen:store("#undef _P_\n\n", 1)
+				end
+
+				gen:store(string.format([[
+		if (ndx == 0 || ndx == 1)
+			p = "%s";
+		break;
+]], v.ret), 2)
+		end
+
+		gen:write(string.format("\t\t*n_args = %d;\n\t\tbreak;\n\t}\n",
+		    n_args))
+		gen:store("\t\tbreak;\n", 1)
+
+		-- Handle compat (everything >= FREEBSD3):
+		-- Do nothing, only for native.
+		end
+	end
+
+	gen:write([[
+	default:
+		*n_args = 0;
+		break;
+	};
+}
+]])
+	gen:store([[
+	default:
+		break;
+	};
+	if (p != NULL)
+		strlcpy(desc, p, descsz);
+}
+]], 1)
+	gen:store([[
+	default:
+		break;
+	};
+	if (p != NULL)
+		strlcpy(desc, p, descsz);
+}
+]], 2)
+
+	-- Write all stored lines.
+	if gen.storage_levels ~= nil then
+		gen:writeStorage()
+	end
+
+end
+
+-- Entry of script:
+if script then
+	local config = require("config")
+
+	if #arg < 1 or #arg > 2 then
+		error("usage: " .. arg[0] .. " syscall.master")
+	end
+
+	local sysfile, configfile = arg[1], arg[2]
+
+	config.merge(configfile)
+	config.mergeCompat()
+	config.mergeCapability()
+
+	-- The parsed system call table.
+	local tbl = FreeBSDSyscall:new{sysfile = sysfile, config = config}
+
+	systrace_args.file = config.systrace -- change file here
+	systrace_args.generate(tbl, config, systrace_args.file)
+end
+
+-- Return the module.
+return systrace_args

sys/tools/syscalls/tools/generator.lua
@@ -1,0 +1,113 @@
+--
+-- SPDX-License-Identifier: BSD-2-Clause
+--
+-- Copyright (c) 2024 Tyler Baxter <agge@FreeBSD.org>
+-- Copyright (c) 2019 Kyle Evans <kevans@FreeBSD.org>
+--
+
+local util = require("tools.util")
+
+local generator = {}
+
+generator.__index = generator
+
+-- Wrapper for Lua write() best practice. For a simpler write call.
+function generator:write(line)
+	assert(self.gen:write(line))
+end
+
+--
+-- A write macro for the PAD64 preprocessor directive.
+-- Used for 32-bit configurations by passing config.abiChanges("pair_64bit") and
+-- padding will be done if necessary.
+--
+-- PARAM: bool, TRUE to pad.
+--
+function generator:pad64(bool)
+	if bool then
+		self:write([[
+#if !defined(PAD64_REQUIRED) && !defined(__amd64__)
+#define PAD64_REQUIRED
+#endif
+]])
+	end
+end
+
+-- Returns the generated tag.
+function generator:tag()
+	return self.tag
+end
+
+generator.storage_levels = {}
+
+-- Optional level to specify which order to store in, which defaults to one if
+-- not provided.
+function generator:store(str, level)
+	level = level or 1
+	self.storage_levels[level] = self.storage_levels[level] or {}
+	table.insert(self.storage_levels[level], str)
+end
+
+-- Write all storage in the order it was stored.
+function generator:writeStorage()
+	if self.storage_levels ~= nil then
+		for _, v in util.ipairsSparse(self.storage_levels) do
+			for _, line in ipairs(v) do
+				generator:write(line)
+			end
+		end
+	end
+end
+
+--
+-- Writes the generated preamble. Default comment is C comments.
+--
+-- PARAM: String str, the title for the file.
+--
+-- PARAM: String comment, nil or optional to change comment (e.g., "#" for sh
+-- comments).
+--
+-- SEE: style(9)
+--
+function generator:preamble(str, comment)
+	if str ~= nil then
+		local comment_start = comment or "/*"
+		local comment_middle = comment or " *"
+		local comment_end = comment or " */"
+		self:write(string.format("%s\n", comment_start))
+		-- Splits our string into lines split by newline, or is just the
+		-- original string if there's no newlines.
+		for line in str:gmatch("[^\n]*") do
+			-- Only add a space after the comment if there's
+			-- text on this line.
+			local space
+			if line ~= "" then
+				space = " "
+			else
+				space = ""
+			end
+			-- Make sure to append the newline back.
+			self:write(string.format("%s%s%s\n", comment_middle,
+			    space, line))
+		end
+		self:write(string.format([[%s
+%s DO NOT EDIT-- this file is automatically %s.
+%s
+
+]], comment_middle, comment_middle, self.tag, comment_end))
+	end
+end
+
+-- generator binds to the parameter file.
+function generator:new(obj, fh)
+	obj = obj or { }
+	setmetatable(obj, self)
+	self.__index = self
+
+	self.gen = assert(io.open(fh, "w+"))
+	self.tag = "@" .. "generated"
+
+	return obj
+end
+
+return generator